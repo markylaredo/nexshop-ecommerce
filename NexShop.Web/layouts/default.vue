f754e73808005cb1092a00808627cb98ddc94be8
MarkAnthony <markanthonylaredo@gmail.com>
2023-06-07 13:07:28 +0800

    Setup vuetify Material Design component

---
+++ NexShop.Web/layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <v-app-bar height="150" color="secondary" flat>
+      <v-container class="fill-height d-flex align-center">
+        <v-avatar class="me-10 ms-4" color="grey-darken-1" size="32"></v-avatar>
+
+        <v-btn v-for="link in links" :key="link" variant="text">
+          {{ link }}
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-responsive max-width="260">
+          <v-text-field
+            density="compact"
+            variant="solo"
+            hide-details
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+    </v-app-bar>
+
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <slot />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    links: ["Dashboard", "Messages", "Profile", "Updates"],
+  }),
+};
+</script>
--- NexShop.Web/nuxt.config.ts
+++ NexShop.Web/nuxt.config.ts
@@ -1,4 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { md1,md3 } from 'vuetify/blueprints'
 export default defineNuxtConfig({
-
+    modules: [
+        '@invictus.codes/nuxt-vuetify'
+    ],
+    vuetify: {
+        vuetifyOptions: {
+            blueprint: md3,
+        },
+        moduleOptions: {
+            treeshaking: true,
+            useIconCDN: true,
+            styles: true,
+            autoImport: true,
+        }
+    }
 })
